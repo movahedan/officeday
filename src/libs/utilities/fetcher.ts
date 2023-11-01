853beec30da5615089043564dedba7362c2e8c29
Soheil Movahedan <soheil.movahhedan@gmail.com>
2023-11-01 17:17:28 +0100

    Add useSWR and fetcher to the project

---
+++ src/libs/utilities/fetcher.ts
@@ -0,0 +1,21 @@
+import { errorHandlerApi } from "./error-handlers";
+
+export const fetcher = (input: RequestInfo, init?: RequestInit | undefined) =>
+  fetch(input, init)
+    .then(async (response) => {
+      const json = await response.json();
+      if (response.ok && response.status < 400 && response.status !== 204) {
+        return json;
+      } else {
+        errorHandlerApi(response);
+        errorHandlerApi(json);
+        throw json;
+      }
+    })
+    .catch((error) => {
+      if (error.error) {
+        throw error.error;
+      } else {
+        throw error;
+      }
+    });
