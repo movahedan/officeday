3146523015e002536483f3100e07c7ede6fb8c84
Soheil Movahedan <soheil.movahhedan@gmail.com>
2023-11-11 23:05:17 +0100

    Fix swagger-ui-react problem by downgrading the version

--- src/app/api-docs/page.tsx
+++ src/app/api-docs/page.tsx
@@ -3,5 +3,7 @@ import "swagger-ui-react/swagger-ui.css";
 import SwaggerUI from "swagger-ui-react";
 
 export default function ApiDocs() {
+  if (!global.window) return null;
+
   return <SwaggerUI url="/swagger.json" />;
 }
