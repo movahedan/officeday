fce54ee12609816c9b1d0b5c95b1f025fd072f3a
Soheil Movahedan <soheil.movahhedan@gmail.com>
2023-11-15 01:06:48 +0100

    Setup PWA

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   distDir: "out/build",
-};
+  experimental: {
+    webpackBuildWorker: true,
+  },
+});
 
 module.exports = nextConfig;
