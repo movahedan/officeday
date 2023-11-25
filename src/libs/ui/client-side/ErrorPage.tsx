b1b1daff7e6baa8d9af4e763f031be6dd8c93cf3
Soheil Movahedan <soheil.movahhedan@gmail.com>
2023-11-25 20:32:00 +0100

    Add reload button for ErrorPage

--- src/libs/ui/client-side/ErrorPage.tsx
+++ src/libs/ui/client-side/ErrorPage.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useTranslations } from "next-intl";
 import { useEffect } from "react";
 
@@ -17,6 +18,7 @@ export function ErrorPage({
   reset?: () => void;
 }) {
   const t = useTranslations("general");
+  const router = useRouter();
 
   useEffect(() => {
     errorHandlerApp(error);
@@ -25,15 +27,20 @@ export function ErrorPage({
   return (
     <div className="w-full my-auto text-center">
       <ErrorComponent error={error} />
-      {reset ? (
-        <Button variant="red" onClick={() => reset()} className="mx-auto mt-16">
-          {t("try-again")}
+
+      <div className="inline-flex mx-auto mt-16 space-x-8">
+        {!!reset && (
+          <Button variant="transparent" onClick={() => reset()}>
+            {t("try-again")}
+          </Button>
+        )}
+        <Button variant="transparent" onClick={() => router.refresh()}>
+          {t("reload")}
         </Button>
-      ) : (
         <Link href={routes.index()}>
-          <Button variant="green">{t("go-home")}</Button>
+          <Button variant="transparent">{t("go-home")}</Button>
         </Link>
-      )}
+      </div>
     </div>
   );
 }
